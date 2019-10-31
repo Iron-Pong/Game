c27b8b0baae6f1d3b81c951ca126532c3c5fa4f4
saruit0105 <Administrator@UM-1WL5462.cgcent.miami.edu>
2019-10-31 14:43:45 -0400

    'm'

--- script.js
+++ script.js
@@ -113,6 +113,23 @@ class Game {
     this.thePlayer2 = new Player(560, 180, 20, 60);
     this.theBall = new Ball(300, 200, 10, 10);
   }
+  collisionDetection() {
+    if (
+      this.theBall.x < this.thePlayer2.x + this.thePlayer2.width &&
+      this.theBall.x + this.theBall.width > this.thePlayer2.x &&
+      this.theBall.y < this.thePlayer2.y + this.thePlayer2.height &&
+      this.theBall.y + this.theBall.height > this.thePlayer2.y
+    ) {
+      console.log("collision made with player 2!");
+    } else if (
+      this.theBall.x < this.thePlayer.x + this.thePlayer.width &&
+      this.theBall.x + this.theBall.width > this.thePlayer.x &&
+      this.theBall.y < this.thePlayer.y + this.thePlayer.height &&
+      this.theBall.y + this.theBall.height > this.thePlayer.y
+    ) {
+      console.log("collision made with player 1!");
+    }
+  }
 }
 
 //Start Button
@@ -124,19 +141,19 @@ function startGame() {
   theGame = new Game();
   mainLoop();
 }
-
-if (
-  theGame.theBall.x < theGame.thePlayer2.x + theGame.thePlayer2.width &&
-  theGame.theBall.x + theGame.theBall.width > theGame.thePlayer2.x &&
-  theGame.theBall.y < theGame.thePlayer2.y + theGame.thePlayer2.height &&
-  theGame.theBall.y + theGame.theBall.height > theGame.thePlayer2.y
-) {
-  console.log("collision made with player 2!");
-} else if (
-  theGame.theBall.x < theGame.thePlayer.x + theGame.thePlayer.width &&
-  theGame.theBall.x + theGame.theBall.width > theGame.thePlayer.x &&
-  theGame.theBall.y < theGame.thePlayer.y + theGame.thePlayer.height &&
-  theGame.theBall.y + theGame.theBall.height > theGame.thePlayer.y
-) {
-  console.log("collision made with player 1!");
-}
+// insert this into collision loop
+// if (
+//   theGame.theBall.x < theGame.thePlayer2.x + theGame.thePlayer2.width &&
+//   theGame.theBall.x + theGame.theBall.width > theGame.thePlayer2.x &&
+//   theGame.theBall.y < theGame.thePlayer2.y + theGame.thePlayer2.height &&
+//   theGame.theBall.y + theGame.theBall.height > theGame.thePlayer2.y
+// ) {
+//   console.log("collision made with player 2!");
+// } else if (
+//   theGame.theBall.x < theGame.thePlayer.x + theGame.thePlayer.width &&
+//   theGame.theBall.x + theGame.theBall.width > theGame.thePlayer.x &&
+//   theGame.theBall.y < theGame.thePlayer.y + theGame.thePlayer.height &&
+//   theGame.theBall.y + theGame.theBall.height > theGame.thePlayer.y
+// ) {
+//   console.log("collision made with player 1!");
+// }
