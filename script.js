211349d2a4a97027d9fdb73c20a5c75eb7df0e1b
robbiesanchez <rfsanchez004@gmail.com>
2019-10-31 11:49:40 -0400

    added player2, paddle controls, movement function

--- script.js
+++ script.js
@@ -9,18 +9,30 @@ class Player{
         this.width = width;
         this.height = height;
     }
+
+// Move Player function
+
+    movePlayer = (direction, value) => {
+        this[direction] += value;
+        console.log(this)
+    }
 }
+
+// Draw Function
 
 function draw(u){
     ctx.fillStyle = 'black';
     ctx.fillRect( u.x, u.y, u.width, u.height);
 }
 
+// Main Loop - runs animation, draws players
+
 function mainLoop(){
     frames++;
     console.log('clearRect has occured');
+    ctx.clearRect(0, 0, 600, 400);
     draw(theGame.thePlayer);
-
+    draw(theGame.thePlayer2);
     requestAnimationFrame(mainLoop);
 }
 
@@ -28,12 +40,43 @@ function mainLoop(){
 
 // }
 
-class Game {
-    //here is where all the classes are called to create the game
-    constructor(){
-        this.thePlayer = new Player(0, 0, 20, 20);
+
+// Speed
+
+let speed = 30;
+
+// Paddle controls
+
+function gameControls(e) {
+    if (e.key === "ArrowUp") {
+        theGame.thePlayer2.movePlayer("y", -speed)
+    }
+    if (e.key === "ArrowDown") {
+        theGame.thePlayer2.movePlayer("y", +speed)
+    }
+    if (e.key === "a") {
+        theGame.thePlayer.movePlayer("y", -speed)
+    }
+    if (e.key === "z") {
+        theGame.thePlayer.movePlayer("y", +speed)
     }
 }
+
+document.onkeypress = gameControls;
+document.onkeydown = gameControls;
+
+
+//here is where all the classes are called to create the game
+class Game {
+    
+    constructor(){
+        this.thePlayer = new Player(20, 180, 20, 60);
+        this.thePlayer2 = new Player(560, 180, 20, 60);
+    }
+}
+
+
+//Start Button
 
 document.getElementById('start-game').onclick = startGame;
 let theGame;
