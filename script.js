449a959d576bf98169114f33a538a6f5611982d7
Avrahm <daosuf@gmail.com>
2019-11-07 10:18:37 -0500

    pull

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var ctx = document.getElementById("game-board").getContext("2d");
 ctx.width = 800;
 ctx.height = 450;
 
-// // // console.log('connected')
+// console.log('connected')
 
 // variables
 
@@ -37,48 +37,41 @@ player2name = urlQuery[1][1]
   .split("+")
   .join(" ")
   .toUpperCase();
-  theme = urlQuery[2][1];
-  
+theme = urlQuery[2][1];
+
 let singlePlayerMode = false;
 
-  if(urlQuery.length===4 && urlQuery[3][0]==="singlePlayerMode"){
+if (urlQuery.length === 4 && urlQuery[3][0] === "singlePlayerMode") {
   singlePlayerMode = true;
-  } else if (urlQuery.length===5) {
-    singlePlayerMode = true;
-  }
-  else {
-    singlePlayerMode = false;
-  }
-
+} else if (urlQuery.length === 5) {
+  singlePlayerMode = true;
+} else {
+  singlePlayerMode = false;
+}
 
 let timerMode = false;
 
-  if(urlQuery.length===4 && urlQuery[3][0]==="timedMode"){
+if (urlQuery.length === 4 && urlQuery[3][0] === "timedMode") {
   timerMode = true;
-  // // console.log(timerMode);
-  } 
-  else if (urlQuery.length===5) {
-    timerMode = true;
-    // // console.log(timerMode); 
-  }
-    else {
-    timerMode = false;
-    document.querySelector("#header > div:nth-child(3)").innerHTML = "";
-    }
+  console.log(timerMode);
+} else if (urlQuery.length === 5) {
+  timerMode = true;
+  console.log(timerMode);
+} else {
+  timerMode = false;
+  document.querySelector("#header > div:nth-child(3)").innerHTML = "";
+}
 
 var vid = document.getElementById("myVideo");
 
 if (theme === "ballsoffury") {
-    vid.play();
-    vid.onended = function() {
-        vid.width = 0;
-        vid.height = 0;
-    }
-} else {vid.width = 0;
-    vid.height = 0;}
-
-
-
+  vid.play();
+  vid.onended = function() {
+    vid.style.display = "block";
+  };
+} else {
+  vid.style.display = "none";
+}
 
 document.body.classList.add(theme);
 document.querySelector("canvas").classList.add(`${theme}-theme`);
@@ -89,7 +82,7 @@ if (singlePlayerMode === "true") {
   singlePlayerToggle = true;
 }
 
-// // // console.log(theme);
+// console.log(theme);
 
 class Player {
   constructor(x, y, width, height) {
@@ -102,26 +95,7 @@ class Player {
   // Move Player function
 
   movePlayer(direction, value) {
-    // // // console.log(direction, value)
-
     this[direction] += value;
-
-    if(Math.sign(value) === 1 ){
-      for(let i=0; i<value; i++){
-        setTimeout(()=>{
-          // // // console.log(this.y)
-          this['y'] += 1
-        },i*5)
-      }
-    }
-    if(Math.sign(value) === -1 ){
-      for(let i=0; i>value; i--){
-        setTimeout(()=>{
-          // // console.log(this.y)
-          this['y'] -= 1
-        },i*5)
-      }
-    }
   }
 
   // Single Player Function... if singlePlayerToggle is True then set the computer(player2) to match the ball y
@@ -155,7 +129,7 @@ class Ball {
 function ballSpeedIncrease() {
   setInterval(() => {
     ballSpeed += 0.5;
-    // // // console.log('ballspeed increase')
+    // console.log('ballspeed increase')
   }, 5500);
 }
 
@@ -191,20 +165,20 @@ function draw(u, object) {
     ctx.fillRect(u.x, u.y, u.width, u.height);
   }
   if (object === "twoBalls") {
-    ctx.drawImage(twoBallsImg, u.x, u.y, u.width, u.height)
+    ctx.drawImage(twoBallsImg, u.x, u.y, u.width, u.height);
   }
 
   if (object === "speedUp") {
-      ctx.drawImage(speedUpImg, u.x, u.y, u.width, u.height)
+    ctx.drawImage(speedUpImg, u.x, u.y, u.width, u.height);
   }
 
   if (object === "barLarge") {
-    ctx.drawImage(barLargeImg, u.x, u.y, u.width, u.height)
-}
+    ctx.drawImage(barLargeImg, u.x, u.y, u.width, u.height);
+  }
 
-if (object === "slowDown") {
-    ctx.drawImage(slowDownImg, u.x, u.y, u.width, u.height)
-    }
+  if (object === "slowDown") {
+    ctx.drawImage(slowDownImg, u.x, u.y, u.width, u.height);
+  }
 
   if (object === "player" && theGame.thePlayer2.y < 0) {
     theGame.thePlayer2.y = 0;
@@ -226,9 +200,9 @@ if (object === "slowDown") {
 // Main Loop - runs animation, draws players
 let frames = 0;
 function mainLoop() {
-  // // // console.log(frames);
+  // console.log(frames);
   frames++;
-  //// // console.log("clearRect has occured");
+  //console.log("clearRect has occured");
   ctx.clearRect(0, 0, ctx.width, ctx.height);
   draw(theGame.thePlayer, "player");
   draw(theGame.thePlayer2, "player");
@@ -238,13 +212,16 @@ function mainLoop() {
   });
   theGame.powerUpsArray.forEach(eachPowerUps => {
     if (eachPowerUps.name === "twoBalls") {
-        draw(eachPowerUps, "twoBalls");
-    } if (eachPowerUps.name === "speedUp") {
-        draw(eachPowerUps, "speedUp");
-    } if (eachPowerUps.name === "barLarge") {
-        draw(eachPowerUps, "barLarge");
-    } if (eachPowerUps.name === "slowDown") {
-        draw(eachPowerUps, "slowDown");
+      draw(eachPowerUps, "twoBalls");
+    }
+    if (eachPowerUps.name === "speedUp") {
+      draw(eachPowerUps, "speedUp");
+    }
+    if (eachPowerUps.name === "barLarge") {
+      draw(eachPowerUps, "barLarge");
+    }
+    if (eachPowerUps.name === "slowDown") {
+      draw(eachPowerUps, "slowDown");
     }
   });
 
@@ -260,13 +237,13 @@ function mainLoop() {
   });
   // theGame.collisionDetection(theGame.theBall.x, theGame.theBall.y);
   theGame.theBallArray.forEach(eachBalls => {
-    // // // console.log(theGame, eachBalls);
+    console.log(theGame, eachBalls);
     theGame.handleCollision(eachBalls);
   });
 
   if (timerMode === false) {
-      gameOver();
-    }
+    gameOver();
+  }
 
   if (isPlaying === true) {
     requestId = requestAnimationFrame(mainLoop);
@@ -275,13 +252,13 @@ function mainLoop() {
 
 // Paddle controls
 function gameControls(e) {
-  if(singlePlayerMode==="false"){
-  if (e.key === "ArrowUp") {
-    theGame.thePlayer2.movePlayer("y", - paddleSpeed);
-  }
-  if (e.key === "ArrowDown") {
-    theGame.thePlayer2.movePlayer("y", + paddleSpeed);
-  }
+  if (singlePlayerMode === false) {
+    if (e.key === "ArrowUp") {
+      theGame.thePlayer2.movePlayer("y", -paddleSpeed);
+    }
+    if (e.key === "ArrowDown") {
+      theGame.thePlayer2.movePlayer("y", +paddleSpeed);
+    }
   }
   if (e.key === "a" || e.key === "A") {
     theGame.thePlayer.movePlayer("y", -paddleSpeed);
@@ -299,17 +276,13 @@ onkeydown = onkeyup = function(e) {
   e = e || event; // to deal with IE
   map[e.key] = e.type == "keydown";
   /* insert conditional here */
-    // // // console.log(e.key);
+  //   console.log(map);
   for (let k in map) {
     if (map[k]) {
       gameControls({ key: k });
     }
   }
 };
-
-// onkeypress = function(e){
-//   // // console.log(e);
-// }
 
 function stop() {
   isPlaying = false;
@@ -349,12 +322,7 @@ let obj = {
 };
 
 //powerup array for the game
-let powerUpsName = [
-  "slowDown",
-   "speedUp",
-  "barLarge",
-  "twoBalls"
-];
+let powerUpsName = ["slowDown", "speedUp", "barLarge", "twoBalls"];
 
 //here is where all the classes are called to create the game
 class Game {
@@ -375,16 +343,17 @@ class Game {
 
     let newPowerUps = new PowerUps(rName, rX, rY, rWidth, rHeight);
     this.powerUpsArray.push(newPowerUps);
-    // // // console.log("Spawning!");
+    // console.log("Spawning!");
   }
 
   clearUnusedPowerUps() {
     this.powerUpsArray.splice(0, 1);
   }
   handleCollision(eachBall) {
-    // // // console.log(eachBall.x, eachBall.y);
+    console.log(eachBall.x, eachBall.y);
     if (
-      eachBall.x < this.thePlayer.x + this.thePlayer.width + eachBall.radius * 1.1 &&
+      eachBall.x <
+        this.thePlayer.x + this.thePlayer.width + eachBall.radius * 1.1 &&
       eachBall.x + eachBall.radius > this.thePlayer.x &&
       eachBall.y < this.thePlayer.y + this.thePlayer.height &&
       eachBall.y + eachBall.radius > this.thePlayer.y
@@ -393,7 +362,8 @@ class Game {
       eachBall.x += 5;
       eachBall.dx *= -1;
     } else if (
-      eachBall.x < this.thePlayer2.x + this.thePlayer2.width + eachBall.radius * 2 &&
+      eachBall.x <
+        this.thePlayer2.x + this.thePlayer2.width + eachBall.radius * 2 &&
       eachBall.x + eachBall.radius * 2 > this.thePlayer2.x &&
       eachBall.y < this.thePlayer2.y + this.thePlayer2.height &&
       eachBall.y + eachBall.radius * 2 > this.thePlayer2.y
@@ -403,56 +373,50 @@ class Game {
       obj[theme + "2"].play();
       // } else if (eachBall.x < 0 || eachBall.x > 800) {
       //   eachBall.dx *= -1;
-      //   // // console.log("hit x walls");
+      //   console.log("hit x walls");
     } else if (eachBall.y < 0 || eachBall.y > 450) {
       eachBall.dy *= -1;
     }
     // Restart ball and keep score
-    let newBallsToPush = 0;
-    for (let i = this.theBallArray.length; i > 0 ; i--) {
+    for (let i = 0; i < this.theBallArray.length; i++) {
       if (eachBall.x < 0) {
         //   stop()
         playerTwoScore += 1;
         ballSpeed = 2;
-        document.querySelector(".player-card #player2 #player-score span").innerText = playerTwoScore;
+        document.querySelector(
+          ".player-card #player2 #player-score span"
+        ).innerText = playerTwoScore;
         obj[theme + "4"].play();
         this.clearUnusedPowerUps();
         let message = `${player2name} Scores!`;
         document.getElementById("game-notification").innerHTML = message;
-        // this.theBallArray.splice(i, this.theBallArray.length);
-        // console.log(this.theBallArray);
-        document.querySelector('canvas').classList.add('score-shake');
-        setTimeout(()=>{
-        document.querySelector('canvas').classList.remove('score-shake');
-        },600)
-        newBallsToPush ++
-        // startGame();
-        for (let j = 0; j > newBallsToPush; i++) {
+        document.querySelector("canvas").classList.add("score-shake");
+        setTimeout(() => {
+          document.querySelector("canvas").classList.remove("score-shake");
+        }, 600);
+        this.theBallArray.splice(i, this.theBallArray.length);
         let newBalls = new Ball(70, 200, -2, 2, ballRadius);
         this.theBallArray.push(newBalls);
-        }
+        // startGame();
       }
       if (eachBall.x > 800) {
         //   stop()
         playerOneScore += 1;
         ballSpeed = 2;
-        document.querySelector(".player-card #player1 #player-score span").innerText = playerOneScore;
+        document.querySelector(
+          ".player-card #player1 #player-score span"
+        ).innerText = playerOneScore;
         obj[theme + "3"].play();
         this.clearUnusedPowerUps();
         let message = `${player1name} Scores!`;
         document.getElementById("game-notification").innerHTML = message;
-        // this.theBallArray.splice(i, this.theBallArray.length);
-        // console.log(this.theBallArray);
-        document.querySelector('canvas').classList.add('score-shake');
-        setTimeout(()=>{
-        document.querySelector('canvas').classList.remove('score-shake');
-        },600)
-        newBallsToPush ++
-        // startGame();
-        for (let j = 0; j > newBallsToPush; i++) {
-        let newBalls = new Ball(70, 200, -2, 2, ballRadius);
+        document.querySelector("canvas").classList.add("score-shake");
+        setTimeout(() => {
+          document.querySelector("canvas").classList.remove("score-shake");
+        }, 600);
+        this.theBallArray.splice(i, this.theBallArray.length);
+        let newBalls = new Ball(760, 200, 2, 2, ballRadius);
         this.theBallArray.push(newBalls);
-        }
         // startGame();
       }
     }
@@ -463,7 +427,7 @@ class Game {
         eachBall.y < this.powerUpsArray[i].y + this.powerUpsArray[i].height &&
         eachBall.y + eachBall.radius > this.powerUpsArray[i].y
       ) {
-        // // // console.log(this.powerUpsArray[i], i);
+        // console.log(this.powerUpsArray[i], i);
         switch (this.powerUpsArray[i].name) {
           case "slowDown":
             ballSpeed = 1;
@@ -495,7 +459,7 @@ class Game {
             break;
         }
         this.powerUpsArray.splice(i, 1);
-        // // // console.log("COLLISION!!");
+        // console.log("COLLISION!!");
       }
     }
   }
@@ -507,8 +471,12 @@ function resetPlayerScores() {
   playerOneScore = 0;
   playerTwoScore = 0;
 
-  document.querySelector(".player-card #player1 #player-score span").innerText = playerOneScore;
-  document.querySelector(".player-card #player2 #player-score span").innerText = playerTwoScore;
+  document.querySelector(
+    ".player-card #player1 #player-score span"
+  ).innerText = playerOneScore;
+  document.querySelector(
+    ".player-card #player2 #player-score span"
+  ).innerText = playerTwoScore;
 }
 
 function countDown() {
@@ -559,69 +527,71 @@ function gameOver() {
 //Timed Game Over Function
 
 function gameOver2() {
-
-    if (playerOneScore > playerTwoScore) {
-        message = `${player1name} WON!`;
-        document.getElementById("game-notification").innerHTML = message;
-        document.getElementById("game-screen-message").innerHTML = `<a onclick="countDown()"> <i class="fas fa-redo"></i></a>`;
-        obj[theme + "5"].play();
-        stop();
-    } else if (playerTwoScore > playerOneScore) {
-        message = `${player2name} WON!`;
-        document.getElementById("game-notification").innerHTML = message;
-        document.getElementById("game-screen-message").innerHTML = `<a onclick="countDown()"> <i class="fas fa-redo"></i></a>`;
-        obj[theme + "5"].play();
-        stop();
-    } else { 
-        message = `${player2name} and ${player1name} TIED!`;
-        document.getElementById("game-notification").innerHTML = message;
-        document.getElementById("game-screen-message").innerHTML = `<a onclick="countDown()"> <i class="fas fa-redo"></i></a>`;
-        obj[theme + "5"].play();
-        stop();
-    }
-
+  if (playerOneScore > playerTwoScore) {
+    message = `${player1name} WON!`;
+    document.getElementById("game-notification").innerHTML = message;
+    document.getElementById(
+      "game-screen-message"
+    ).innerHTML = `<a onclick="countDown()"> <i class="fas fa-redo"></i></a>`;
+    obj[theme + "5"].play();
+    stop();
+  } else if (playerTwoScore > playerOneScore) {
+    message = `${player2name} WON!`;
+    document.getElementById("game-notification").innerHTML = message;
+    document.getElementById(
+      "game-screen-message"
+    ).innerHTML = `<a onclick="countDown()"> <i class="fas fa-redo"></i></a>`;
+    obj[theme + "5"].play();
+    stop();
+  } else {
+    message = `${player2name} and ${player1name} TIED!`;
+    document.getElementById("game-notification").innerHTML = message;
+    document.getElementById(
+      "game-screen-message"
+    ).innerHTML = `<a onclick="countDown()"> <i class="fas fa-redo"></i></a>`;
+    obj[theme + "5"].play();
+    stop();
+  }
 }
 
 // Timer Function
 
-
 function timerToggle() {
-    if (timerMode === true) {
-        timerFuction();
-    } else {
-        gameOver();
-    }
+  if (timerMode === true) {
+    timerFuction();
+  } else {
+    gameOver();
+  }
 }
 
-function timerFuction(){
-    let counter1 = theTimer;
-    // let counter2 = 0;
-    
-    let timer1 = setInterval(function() {
-      startCountUp(counter1);
-    }, 1000);
-    
-    // let timer2 = setInterval(function() {
-    //     startCountUp(counter2);
-    //   }, 100);
-  
-    function startCountUp() {
-      if (counter1 === 0) {
-        
-        clearInterval(timer1);
-        // clearInterval(timer2);
-        document.querySelector("#header > div:nth-child(3) > span").innerHTML = 0;
-        gameOver2();
-      } else {
-        counter1--;
-        document.querySelector("#header > div:nth-child(3) > span").innerHTML = counter1 
+function timerFuction() {
+  let counter1 = theTimer;
+  // let counter2 = 0;
+
+  let timer1 = setInterval(function() {
+    startCountUp(counter1);
+  }, 1000);
+
+  // let timer2 = setInterval(function() {
+  //     startCountUp(counter2);
+  //   }, 100);
+
+  function startCountUp() {
+    if (counter1 === 0) {
+      clearInterval(timer1);
+      // clearInterval(timer2);
+      document.querySelector("#header > div:nth-child(3) > span").innerHTML = 0;
+      gameOver2();
+    } else {
+      counter1--;
+      document.querySelector(
+        "#header > div:nth-child(3) > span"
+      ).innerHTML = counter1;
     }
   }
 }
 
-  //
-
-
+//
 
 // var startTime = Date.now();
 // function countUp() {
@@ -634,7 +604,6 @@ function timerFuction(){
 //             }
 //     }, 100);
 // }
-
 
 //Start Button
 
